5b6b6d5593c92cfcb3f89a2af01ebab8da5b1ad9
ZhichenRen <1337795211@qq.com>
2021-11-05 22:02:18 +0800

    add request template

--- hello.go
+++ hello.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
+	"strconv"
 )
 
 var rdb *redis.Client
@@ -49,34 +49,96 @@ func redisExample() {
 	}
 }
 
+func SnatchHandler(c *gin.Context){
+	userId, _ := c.GetPostForm("uid")
+	fmt.Println(userId)
+	envelopeId := 123
+	maxCount := 5
+	curCount := 3
+
+	c.JSON(200, gin.H{
+		"code": 0,
+		"msg": "success",
+		"data": gin.H{
+			"envelop_id": envelopeId,
+			"max_count": maxCount,
+			"cur_count" : curCount,
+		},
+	})
+}
+
+func OpenHandler(c *gin.Context){
+	userId, _ := c.GetPostForm("uid")
+	fmt.Println(userId)
+
+	value := 50
+
+	c.JSON(200, gin.H{
+		"code": 0,
+		"msg": "success",
+		"data": gin.H{
+			"value": value,
+		},
+	})
+}
+
+func WalletListHandler(c *gin.Context){
+	userId, _ := c.GetPostForm("uid")
+	fmt.Println(userId)
+
+	envelops := []gin.H{
+		{
+			"envelop_id": 123,
+			"value": 50,
+			"opened": true,
+			"snatch_time": 1634551711,
+		},
+	}
+	amount := 50
+
+	c.JSON(200, gin.H{
+		"code": 0,
+		"msg": "success",
+		"data": gin.H{
+			"amount": amount,
+			"envelope_list": envelops,
+		},
+	})
+}
+
+func Ping(c* gin.Context){
+	countString, getError := rdb.Get("count").Result()
+	var value int
+	if getError == nil {
+		count, countError := strconv.Atoi(countString)
+		if countError != nil {
+			fmt.Println(countError)
+		}
+		value = count + 1
+		setError := rdb.Set("count", strconv.Itoa(value), 0).Err()
+		if setError != nil {
+			fmt.Println(setError)
+		}
+	} else {
+		fmt.Println(getError)
+		setError := rdb.Set("count", strconv.Itoa(1), 0).Err()
+		if setError != nil {
+			fmt.Println(setError)
+		}
+		value = 1
+	}
+	c.JSON(200, gin.H{
+		"message": "这个网页已经被访问了" + strconv.Itoa(value) + "次！",
+	})
+}
+
 func main() {
 	initClient()
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		countString, getError := rdb.Get("count").Result()
-		var value int
-		if getError == nil {
-			count, countError := strconv.Atoi(countString)
-			if countError != nil {
-				fmt.Println(countError)
-			}
-			value = count + 1
-			setError := rdb.Set("count", strconv.Itoa(value), 0).Err()
-			if setError != nil {
-				fmt.Println(setError)
-			}
-		} else {
-			fmt.Println(getError)
-			setError := rdb.Set("count", strconv.Itoa(1), 0).Err()
-			if setError != nil {
-				fmt.Println(setError)
-			}
-			value = 1
-		}
-		c.JSON(200, gin.H{
-			"message": "这个网页已经被访问了" + strconv.Itoa(value) + "次！",
-		})
-	})
+	r.GET("/ping", Ping)
+	r.POST("/snatch", SnatchHandler)
+	r.POST("/open", OpenHandler)
+	r.POST("/get_wallet_list", WalletListHandler)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
