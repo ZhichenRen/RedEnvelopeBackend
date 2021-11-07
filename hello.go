697ea498123fb4d851b2583266bf19a3926de55b
ZhichenRen <1337795211@qq.com>
2021-11-07 16:16:43 +0800

    update snatch handler

--- hello.go
+++ hello.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
-	"math/rand"
+	"go-web/database"
 	"strconv"
 	"time"
 )
@@ -42,16 +41,49 @@ func initClient() (err error) {
 
 func SnatchHandler(c *gin.Context){
 	userId, _ := c.GetPostForm("uid")
-	fmt.Println(userId)
-
-	randNumber := rand.Intn(100)
+	user, err := rdb.HGetAll("User:" + userId).Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+	// 后期与mysql对接，不存在时读取用户信息
+	if len(user) == 0 {
+		userInfo := make(map[string]interface{})
+		userInfo["CurCount"] = 0
+		userInfo["Amount"] = 0
+		rdb.HMSet("User:" + userId, userInfo)
+		user, err = rdb.HGetAll("User:" + userId).Result()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	fmt.Println("User:", user)
+	maxCount, err := rdb.Get("MaxCount").Result()
 	//随机数判断用户是否抢到红包，后期需要替换
-	if randNumber < 5 {
+	if user["CurCount"] < maxCount{
 		snatchTime := time.Now().Unix()
-		fmt.Println(snatchTime)
-		envelopeId := 123
-		maxCount := 5
-		curCount := 3
+		envelopeId, err := rdb.Incr("EnvelopeId").Result()
+		if err != nil {
+			fmt.Println(err)
+		}
+		curCount, err := rdb.HIncrBy("User:" + userId, "CurCount", 1).Result()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		envelope := make(map[string]interface{})
+		envelope["Value"] = 0
+		envelope["Opened"] = false
+		envelope["SnatchTime"] = snatchTime
+		_, err = rdb.HMSet("Envelope:" + strconv.Itoa(int(envelopeId)), envelope).Result()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Envelope:", envelope)
+
+		_, err = rdb.LPush("User:" + userId + ":Envelopes", envelopeId).Result()
+		if err != nil {
+			fmt.Println(err)
+		}
 		c.JSON(200, gin.H{
 			"code": 0,
 			"msg": "success",
@@ -109,62 +141,54 @@ func WalletListHandler(c *gin.Context){
 }
 
 func Ping(c* gin.Context){
-	countString, getError := rdb.Get("count").Result()
-	var value int
-	if getError == nil {
-		count, countError := strconv.Atoi(countString)
-		if countError != nil {
-			fmt.Println(countError)
-		}
-		value = count + 1
-		setError := rdb.Set("count", strconv.Itoa(value), 0).Err()
-		if setError != nil {
-			fmt.Println(setError)
-		}
-	} else {
-		fmt.Println(getError)
-		setError := rdb.Set("count", strconv.Itoa(1), 0).Err()
-		if setError != nil {
-			fmt.Println(setError)
-		}
-		value = 1
-	}
-	c.JSON(200, gin.H{
-		"message": "这个网页已经被访问了" + strconv.Itoa(value) + "次！",
-	})
-}
-
-func main() {
-	initClient()
-	//r := gin.Default()
-	//r.GET("/ping", Ping)
-	//r.POST("/snatch", SnatchHandler)
-	//r.POST("/open", OpenHandler)
-	//r.POST("/get_wallet_list", WalletListHandler)
-	//r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-
-	envelopeId , err := rdb.Get("EnvelopeId").Result()
+	_, err := rdb.Get("Count").Result()
 	if err == nil {
-		rdb.Incr("EnvelopeId")
-		envelope := &Envelope{
-			EnvelopeId: envelopeId,
-			Value: 0,
-			Opened: false,
-			SnatchTime: time.Now().Unix(),
-		}
-		data, _ := json.Marshal(envelope)
-		err := rdb.Set(envelopeId, data, -1).Err()
+		result, err := rdb.Incr("count").Result()
 		if err != nil {
 			fmt.Println(err)
 		}
+		c.JSON(200, gin.H{
+			"message": "这个网页已经被访问了" + strconv.Itoa(int(result)) + "次。",
+		})
+	} else {
+		rdb.Set("Count", 1, 0)
+		c.JSON(200, gin.H{
+			"message": "这个网页已经被访问了" + strconv.Itoa(1) + "次。",
+		})
 	}
+}
 
-	envelopeJson, err := rdb.Get("100000000000").Result()
-	if err == nil {
-		var envelope Envelope
-		json.Unmarshal([]byte(envelopeJson), &envelope)
-		fmt.Println(envelope)
-	}
+func main() {
+	rdb = database.InitRedisClient(rdb)
+	r := gin.Default()
+	r.GET("/ping", Ping)
+	r.POST("/snatch", SnatchHandler)
+	r.POST("/open", OpenHandler)
+	r.POST("/get_wallet_list", WalletListHandler)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	//envelopeId , err := rdb.Get("EnvelopeId").Result()
+	//if err == nil {
+	//	rdb.Incr("EnvelopeId")
+	//	envelope := &Envelope{
+	//		EnvelopeId: envelopeId,
+	//		Value: 0,
+	//		Opened: false,
+	//		SnatchTime: time.Now().Unix(),
+	//	}
+	//	data, _ := json.Marshal(envelope)
+	//	err := rdb.Set(envelopeId, data, -1).Err()
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
+	//}
+	//
+	//envelopeJson, err := rdb.Get("100000000000").Result()
+	//if err == nil {
+	//	var envelope Envelope
+	//	json.Unmarshal([]byte(envelopeJson), &envelope)
+	//	fmt.Println(envelope)
+	//}
 
 }
 
