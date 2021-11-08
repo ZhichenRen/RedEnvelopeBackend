c2eff7f9148c98497c228e3625447467488485df
ZhichenRen <1337795211@qq.com>
2021-11-08 10:43:18 +0800

    add rocketmq test

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-web
 go 1.16
 
 require (
+	github.com/apache/rocketmq-client-go/v2 v2.1.0
 	github.com/gin-gonic/gin v1.7.1
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/onsi/ginkgo v1.16.5 // indirect
--- hello.go
+++ hello.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/apache/rocketmq-client-go/v2"
+	"github.com/apache/rocketmq-client-go/v2/consumer"
+	"github.com/apache/rocketmq-client-go/v2/primitive"
+	"github.com/apache/rocketmq-client-go/v2/producer"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
 	"go-web/database"
 	"math/rand"
+	"os"
 	"strconv"
 	"time"
 )
@@ -174,6 +180,126 @@ func Ping(c* gin.Context){
 	}
 }
 
+func Producer(c *gin.Context) {
+	addr,err := primitive.NewNamesrvAddr("http://MQ_INST_8149062485579066312_2586445845.cn-beijing.rocketmq-internal.ivolces.com:24009")
+	if err != nil {
+		panic(err)
+	}
+	topic := "Msg"
+	p,err := rocketmq.NewProducer(
+		producer.WithGroupName("GID_Group"),
+		producer.WithNameServer(addr),
+		producer.WithCreateTopicKey(topic),
+		producer.WithRetry(1))
+	if err != nil {
+		panic(err)
+	}
+
+	err = p.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	// 发送异步消息
+	res,err := p.SendSync(context.Background(),primitive.NewMessage(topic,[]byte("send sync message")))
+	if err != nil {
+		fmt.Printf("send sync message error:%s\n",err)
+	} else {
+		fmt.Printf("send sync message success. result=%s\n",res.String())
+	}
+
+	// 发送消息后回调
+	err = p.SendAsync(context.Background(), func(ctx context.Context, result *primitive.SendResult, err error) {
+		if err != nil {
+			fmt.Printf("receive message error:%v\n",err)
+		} else {
+			fmt.Printf("send message success. result=%s\n",result.String())
+		}
+	},primitive.NewMessage(topic,[]byte("send async message")))
+	if err != nil {
+		fmt.Printf("send async message error:%s\n",err)
+	}
+
+	// 批量发送消息
+	var msgs []*primitive.Message
+	for i := 0; i < 5; i++ {
+		msgs = append(msgs, primitive.NewMessage(topic,[]byte("batch send message. num:"+strconv.Itoa(i))))
+	}
+	res,err = p.SendSync(context.Background(),msgs...)
+	if err != nil {
+		fmt.Printf("batch send sync message error:%s\n",err)
+	} else {
+		fmt.Printf("batch send sync message success. result=%s\n",res.String())
+	}
+
+	// 发送延迟消息
+	msg := primitive.NewMessage(topic,[]byte("delay send message"))
+	msg.WithDelayTimeLevel(3)
+	res,err = p.SendSync(context.Background(),msg)
+	if err != nil {
+		fmt.Printf("delay send sync message error:%s\n",err)
+	} else {
+		fmt.Printf("delay send sync message success. result=%s\n",res.String())
+	}
+
+	// 发送带有tag的消息
+	msg1 := primitive.NewMessage(topic,[]byte("send tag message"))
+	msg1.WithTag("tagA")
+	res,err = p.SendSync(context.Background(),msg1)
+	if err != nil {
+		fmt.Printf("send tag sync message error:%s\n",err)
+	} else {
+		fmt.Printf("send tag sync message success. result=%s\n",res.String())
+	}
+
+	err = p.Shutdown()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Consumer(c *gin.Context) {
+	topic := "Msg"
+
+	// 消息主动推送给消费者
+	c2,err := rocketmq.NewPushConsumer(
+		consumer.WithGroupName("GID_Group"),
+		consumer.WithNsResolver(primitive.NewPassthroughResolver([]string{"http://MQ_INST_8149062485579066312_2586445845.cn-beijing.rocketmq-internal.ivolces.com:24009"})),
+		consumer.WithConsumeFromWhere(consumer.ConsumeFromFirstOffset), // 选择消费时间(首次/当前/根据时间)
+		consumer.WithConsumerModel(consumer.BroadCasting)) // 消费模式(集群消费:消费完其他人不能再读取/广播消费：所有人都能读)
+	if err != nil {
+		panic(err)
+	}
+
+	err = c2.Subscribe(
+		topic,consumer.MessageSelector{
+			Type: consumer.TAG,
+			Expression: "*", // 可以 TagA || TagB
+		},
+		func(ctx context.Context, msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
+			orderlyCtx,_ := primitive.GetOrderlyCtx(ctx)
+			fmt.Printf("orderly context: %v\n",orderlyCtx)
+			for i := range msgs {
+				fmt.Printf("Subscribe callback: %v\n",msgs[i])
+			}
+			return consumer.ConsumeSuccess,nil
+		})
+	if err != nil {
+		fmt.Printf("Subscribe error:%s\n",err)
+	}
+
+	err = c2.Start()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	time.Sleep(time.Minute)
+	err = c2.Shutdown()
+	if err != nil {
+		fmt.Println("Shutdown Consumer error: ",err)
+	}
+}
+
 func main() {
 	rdb = database.InitRedisClient(rdb)
 	r := gin.Default()
@@ -181,6 +307,8 @@ func main() {
 	r.POST("/snatch", SnatchHandler)
 	r.POST("/open", OpenHandler)
 	r.POST("/get_wallet_list", WalletListHandler)
+	r.GET("/produce", Producer)
+	r.GET("/consume", Consumer)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
