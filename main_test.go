cb8cfedb7ec72edbe41da7bf3048b94f2e878848
ZhichenRen <1337795211@qq.com>
2021-11-14 14:28:33 +0800

    add unit test

--- main_test.go
+++ main_test.go
@@ -26,9 +26,19 @@ func TestSnatchHandler(t *testing.T) {
 }
 
 func TestOpenHandler(t *testing.T) {
-
+	router := setupRouter()
+	w := httptest.NewRecorder()
+	body := bytes.NewReader([]byte("uid=4444&envelope_id=1000000342"))
+	req, _ := http.NewRequest("POST", "/open", body)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
 }
 
 func TestWalletListHandler(t *testing.T) {
-
+	router := setupRouter()
+	w := httptest.NewRecorder()
+	body := bytes.NewReader([]byte("uid=1"))
+	req, _ := http.NewRequest("POST", "/get_wallet_list", body)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
 }
