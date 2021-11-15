2397fdf24145056b2b6f8b5dc80350233b0669b4
LOTEAT <zengle@gmail.com>
2021-11-15 14:32:41 +0800

    token budget

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 	"go-web/allocate"
 	"go-web/handler"
@@ -9,22 +10,20 @@ import (
 	"time"
 )
 
-func setupRouter() *gin.Engine{
+func main() {
 	allocate.Init()
 	handler.InitClient()
 	handler.InitProducer()
 	fmt.Println(handler.GetProducer())
 	r := gin.Default()
-	r.Use(tokenbucket.NewLimiter(10000, 10000, 500*time.Millisecond))
+	limit1 := tokenbucket.NewLimiter(2000, 2000, 500*time.Millisecond)
+	limit2 := tokenbucket.NewLimiter(2000, 2000, 500*time.Millisecond)
+	limit3 := tokenbucket.NewLimiter(2000, 2000, 500*time.Millisecond)
+	pprof.Register(r)
 	r.GET("/ping", handler.Ping)
-	r.POST("/snatch", handler.SnatchHandler)
-	r.POST("/open", handler.OpenHandler)
-	r.POST("/get_wallet_list", handler.WalletListHandler)
-	return r
-}
-
-func main() {
-	r := setupRouter()
+	r.POST("/snatch", limit1, handler.SnatchHandler)
+	r.POST("/open", limit2, handler.OpenHandler)
+	r.POST("/get_wallet_list", limit3, handler.WalletListHandler)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	handler.CloseProducer()
 }
